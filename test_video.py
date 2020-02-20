296ece893f8f73d36e6439ab686889c805d69c1e
CraigWang1 <55901251+CraigWang1@users.noreply.github.com>
2020-02-19 17:30:10 -0800

    Update test_video.py

--- test_video.py
+++ test_video.py
@@ -20,9 +20,13 @@ def get_args():
 
 
 def test(opt):
-    model = torch.load(opt.pretrained_model).module
+    #load model
+    checkpoint = torch.load(pretrained_model)
+    model = EfficientDet(num_classes=1)  #initialize model
+    model.load_state_dict(checkpoint['state_dict'])  #load state_dict
+
     if torch.cuda.is_available():
-        model.cuda()
+    model.cuda()
 
     cap = cv2.VideoCapture(opt.input)
     out = cv2.VideoWriter(opt.output,  cv2.VideoWriter_fourcc(*"MJPG"), int(cap.get(cv2.CAP_PROP_FPS)),
